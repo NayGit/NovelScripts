02320257811fb0fdadb539d35c421591afbed600
NayGit <96684864+NayGit@users.noreply.github.com>
2022-04-24 16:54:56 +0300

    WebPack v0.0.3: Build, Watch . gitignore: Webpack_Test

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path');
 
-module.exports = {
-    entry: {
-        WebnovelCom_Crawler: './src/WebnovelCom_Crawler.js',
-        WebnovelCom_GetText: './src/WebnovelCom_GetText.js',
-        WebnovelCom_Replace: './src/WebnovelCom_Replace.js',
-        WebnovelCom_Tag: './src/WebnovelCom_Tag.js',
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.user.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    optimization: {
-        minimize: false
-    }
+module.exports = (env) => {
+    return {
+        entry: {
+            WebnovelCom_Crawler: './src/WebnovelCom_Crawler.js',
+            WebnovelCom_GetText: './src/WebnovelCom_GetText.js',
+            WebnovelCom_Replace: './src/WebnovelCom_Replace.js',
+            WebnovelCom_Tag: './src/WebnovelCom_Tag.js',
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: env.test ? 'Test.[name].bundle.user.js' : '[name].bundle.user.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        optimization: {
+            minimize: false
+        }
+    };
 };
